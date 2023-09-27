53fd8d62b8f389f9a238316e7e3928b268389f1b
Kevin Le <iamkevinvle@gmail.com>
2023-09-27 12:43:23 -0700

    edit json output of openAI post route

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -7,12 +7,25 @@ const openai = new OpenAI({
 
 export async function POST(request: Request) {
   const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: "List out the last 10 NBA MVP's." }],
+    messages: [
+      // {
+      //   role: 'system',
+      //   content:
+      //     'You are a Spotify AI Assistant tasked with helping users make personalized Spotify playlists when given a genre of music and/or 3 artists that they like.',
+      // },
+      { role: 'user', content: 'Generate me a 10 song playlist.' },
+    ],
     model: 'gpt-3.5-turbo',
   });
+  // .catch((err) => {
+  //   if (err instanceof OpenAI.APIError) {
+  //     console.log(err.status);
+  //     console.log(err.name);
+  //     console.log(err.headers);
+  //   } else {
+  //     throw err;
+  //   }
+  // });
 
-  return NextResponse.json({
-    status: 200,
-    data: chatCompletion.choices,
-  });
+  return NextResponse.json({ data: chatCompletion.choices });
 }
