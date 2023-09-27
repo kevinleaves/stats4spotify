8a1efb61fc9d55e5faf8ae7597972321a8078d62
Kevin Le <iamkevinvle@gmail.com>
2023-09-27 12:42:46 -0700

    add openai service

---
+++ app/(client)/(services)/openai-service.ts
@@ -0,0 +1,18 @@
+const baseURL = '/api/openai';
+
+export const getPlaylist = async (input: string) => {
+  const body = {
+    query: input,
+  };
+
+  const options: RequestInit = {
+    method: 'POST',
+    body: JSON.stringify(body),
+  };
+
+  const response = await fetch(baseURL, options);
+
+  const data = await response.json();
+
+  return data.data[0].message.content;
+};
