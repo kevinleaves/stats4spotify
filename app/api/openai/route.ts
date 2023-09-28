f26cc656714c7f0183beca9828c6aab28188c943
Kevin Le <iamkevinvle@gmail.com>
2023-09-28 11:09:39 -0700

    add packages

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -6,26 +6,21 @@ const openai = new OpenAI({
 });
 
 export async function POST(request: Request) {
+  const body = await request.json();
   const chatCompletion = await openai.chat.completions.create({
     messages: [
-      // {
-      //   role: 'system',
-      //   content:
-      //     'You are a Spotify AI Assistant tasked with helping users make personalized Spotify playlists when given a genre of music and/or 3 artists that they like.',
-      // },
-      { role: 'user', content: 'Generate me a 10 song playlist.' },
+      {
+        role: 'system',
+        content:
+          'You are a Spotify AI Assistant tasked with helping users make personalized Spotify playlists when given a genre of music and/or 3 artists that they like.',
+      },
+      {
+        role: 'user',
+        content: `Generate me a 10 song playlist consisting of songs from artists: ${body.query}, in an array of JSON objects with the following shape example: {title: 'Goodies', artist: Ciara} `,
+      },
     ],
     model: 'gpt-3.5-turbo',
   });
-  // .catch((err) => {
-  //   if (err instanceof OpenAI.APIError) {
-  //     console.log(err.status);
-  //     console.log(err.name);
-  //     console.log(err.headers);
-  //   } else {
-  //     throw err;
-  //   }
-  // });
 
   return NextResponse.json({ data: chatCompletion.choices });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
-
+import Input from './(client)/(widgets)/(input)/Input';
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <h1>hi</h1>
+      <Input />
+    </main>
   );
 }
