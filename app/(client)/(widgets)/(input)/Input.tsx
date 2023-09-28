7dff1371746cafdaa6fa099ee149a42e50d62a60
Kevin Le <iamkevinvle@gmail.com>
2023-09-28 11:06:14 -0700

    add spotify login button

--- app/(client)/(widgets)/(input)/Input.tsx
+++ app/(client)/(widgets)/(input)/Input.tsx
@@ -1,21 +1,32 @@
 'use client';
 import { useState } from 'react';
-import { getPlaylist } from '../(services)/openai-service';
+import { getPlaylist } from '../../(services)/openai-service';
 
 export default function Input() {
   const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [results, setResults] = useState('');
 
   async function onSubmit(event: React.SyntheticEvent) {
     event.preventDefault();
+    setIsLoading(true);
     const res = await getPlaylist(input);
     setResults(res);
+    setIsLoading(false);
+    setInput('');
   }
+
+  const login = async () => {
+    const res = await fetch('/api/spotify/login');
+    const data = await res.json();
+    console.log(data, 'data');
+  };
 
   return (
     <section>
+      {isLoading ? <div>loading </div> : <div>not loading</div>}
       <div>{results}</div>
-      <div>yerr</div>
+      <button onClick={login}>log into spotify</button>
       <form onSubmit={onSubmit}>
         <input
           className="rounded-lg text-black w-full h-12"
