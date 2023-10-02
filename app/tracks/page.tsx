fb23a04e4990c17c7775582f7e1ba5aa6128e805
Kevin Le <iamkevinvle@gmail.com>
2023-10-02 15:15:12 -0700

    add audio features into track object

--- app/tracks/page.tsx
+++ app/tracks/page.tsx
@@ -1,5 +1,6 @@
-import { getUsersTopItems } from '@/lib/spotify';
+import { getTracksAudioFeatures, getUsersTopItems } from '@/lib/spotify';
 import Image from 'next/image';
+
 interface Props {}
 
 export default async function TracksPage({}: Props) {
@@ -12,6 +13,24 @@ export default async function TracksPage({}: Props) {
     return artistNames.join(', ');
   };
 
+  const getTrackIds = (tracks: SpotifyApi.TrackObjectFull[]) => {
+    return tracks.map((tracks) => tracks.id).join(',');
+  };
+
+  const { audio_features: features } = await getTracksAudioFeatures(
+    getTrackIds(tracks)
+  );
+
+  const addMetadataToTracks = (
+    tracks: SpotifyApi.TrackObjectFull[],
+    features: SpotifyApi.AudioFeaturesObject[]
+  ) => {
+    for (let i = 0; i < tracks.length; i++) {
+      Object.assign(tracks[i], features[i]);
+    }
+  };
+  addMetadataToTracks(tracks, features);
+
   return (
     <>
       {tracks.map((track) => (
@@ -19,6 +38,7 @@ export default async function TracksPage({}: Props) {
           <p>{track.name}</p>
           <p>{track.href}</p>
           <p>{getArtistString(track.artists)}</p>
+          <p>{Math.round(track.tempo)}</p>
           <Image
             src={track.album.images[2].url}
             alt="a track image"
