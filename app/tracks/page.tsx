a75cc17310de011dd05173fd259d3d4a756bc695
Kevin Le <iamkevinvle@gmail.com>
2023-10-02 13:55:28 -0700

    new tracks route/page

---
+++ app/tracks/page.tsx
@@ -0,0 +1,32 @@
+import { getUsersTopItems } from '@/lib/spotify';
+import Image from 'next/image';
+interface Props {}
+
+export default async function TracksPage({}: Props) {
+  const response = await getUsersTopItems('tracks', 'short_term', 50);
+
+  const { items: tracks }: { items: SpotifyApi.TrackObjectFull[] } = response;
+
+  const getArtistString = (artists: SpotifyApi.ArtistObjectSimplified[]) => {
+    const artistNames = artists.map((artist) => artist.name);
+    return artistNames.join(', ');
+  };
+
+  return (
+    <>
+      {tracks.map((track) => (
+        <li key={track.id}>
+          <p>{track.name}</p>
+          <p>{track.href}</p>
+          <p>{getArtistString(track.artists)}</p>
+          <Image
+            src={track.album.images[2].url}
+            alt="a track image"
+            width={50}
+            height={50}
+          />
+        </li>
+      ))}
+    </>
+  );
+}
