71fa9eeabc66cdc9c00e71952363f2ab20941183
Kevin Le <iamkevinvle@gmail.com>
2023-10-06 21:48:16 -0700

    wrap tracks list in suspense component

--- app/(client)/tracks/top/page.tsx
+++ app/(client)/tracks/top/page.tsx
@@ -1,6 +1,8 @@
 import { getTracksAudioFeatures, getUsersTopItems } from '@/lib/spotify';
 import Image from 'next/image';
 import ExportPlaylistButton from '../components/ExportPlaylistButton';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 interface Props {
   params: { slug: string };
@@ -63,33 +65,36 @@ export default async function TracksPage({ searchParams }: Props) {
   return (
     <main className="flex flex-col justify-center items-center gap-20">
       <h2 className="text-3xl">{headerText}</h2>
-      <ExportPlaylistButton headerText={headerText} uris={trackUris} />
-      <ul className="flex flex-col gap-4">
-        {tracks.map((track, index) => (
-          <li key={track.id} className="flex gap-4">
-            <p className="font-bold text-md self-center">{index + 1}</p>
-            <Image
-              src={track.album.images[1].url}
-              alt="a track image"
-              width={50}
-              height={50}
-              className="rounded-xl w-24"
-            />
-            <div className="flex justify-between w-full">
-              <div className="flex-col mr-">
-                <p className="text-xl font-semibold">{track.name}</p>
-                <p className="font-extralight">
-                  {getArtistString(track.artists)}
-                </p>
-                <p className="font-extralight">
-                  BPM/Tempo: {Math.round(track.tempo)}
-                </p>
+
+      <Suspense key={searchParams.timeRange} fallback={<Loading />}>
+        <ExportPlaylistButton headerText={headerText} uris={trackUris} />
+        <ul className="flex flex-col gap-4">
+          {tracks.map((track, index) => (
+            <li key={track.id} className="flex gap-4">
+              <p className="font-bold text-md self-center">{index + 1}</p>
+              <Image
+                src={track.album.images[1].url}
+                alt="a track image"
+                width={50}
+                height={50}
+                className="rounded-xl w-24"
+              />
+              <div className="flex justify-between w-full">
+                <div className="flex-col mr-">
+                  <p className="text-xl font-semibold">{track.name}</p>
+                  <p className="font-extralight">
+                    {getArtistString(track.artists)}
+                  </p>
+                  <p className="font-extralight">
+                    BPM/Tempo: {Math.round(track.tempo)}
+                  </p>
+                </div>
+                <a href={track.uri}>track link</a>
               </div>
-              <a href={track.uri}>track link</a>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </Suspense>
     </main>
   );
 }
