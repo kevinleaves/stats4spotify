7ee58f30910f0f652df511b6695576ea1357104f
Kevin Le <iamkevinvle@gmail.com>
2023-10-07 20:57:00 -0700

    add spotify icon to artist card

--- app/(client)/artists/components/ArtistCard.tsx
+++ app/(client)/artists/components/ArtistCard.tsx
@@ -1,24 +1,40 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import ListItem from '@mui/material/ListItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 interface Props {
   artist: SpotifyApi.ArtistObjectFull;
 }
 
 export default function ArtistCard({ artist }: Props) {
+  const {
+    external_urls: { spotify: artistLink },
+  } = artist;
   return (
     <ListItem className="flex flex-col" key={artist.id}>
-      <div>id: {artist.id}</div>
-      <div>{artist.name}</div>
-      <div>{artist.popularity}</div>
-      <div>{JSON.stringify(artist.genres)}</div>
-      <div>{JSON.stringify(artist.images)}</div>
       <Image
         src={artist.images[1].url}
         alt={'spotify artist image'}
         width={320}
         height={320}
       />
+      {/* <div>id: {artist.id}</div> */}
+      <div>{artist.name}</div>
+      {/* <div>{JSON.stringify(artist.genres)}</div> */}
+      {/* <div>{JSON.stringify(artist.images)}</div> */}
+      <ul className="flex gap-2">
+        {artist.genres.map((genre) => (
+          <li className="font-extralight">{genre}</li>
+        ))}
+      </ul>
+      <div className="flex justify-between gap-4">
+        <div className="font-extralight">popularity: {artist.popularity}</div>
+        <Link href={artistLink} target="_blank">
+          <FontAwesomeIcon className={'h-8'} icon={fab.faSpotify} />
+        </Link>
+      </div>
     </ListItem>
   );
 }
