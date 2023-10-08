4ed208de382aae066f663c23bd4a5b69ad7f312a
Kevin Le <iamkevinvle@gmail.com>
2023-10-07 20:58:52 -0700

    add list render animations via framer motion

--- app/(client)/tracks/components/TrackList.tsx
+++ app/(client)/tracks/components/TrackList.tsx
@@ -1,42 +1,64 @@
+'use client';
+
 import Image from 'next/image';
 import { getTracksAudioFeatures, getUsersTopItems } from '@/lib/spotify';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface Props {
   tracks: SpotifyApi.TrackObjectFull[];
 }
 
-export default async function TrackList({ tracks }: Props) {
+export default function TrackList({ tracks }: Props) {
   const getArtistString = (artists: SpotifyApi.ArtistObjectSimplified[]) => {
     const artistNames = artists.map((artist) => artist.name);
     return artistNames.join(', ');
   };
 
+  const variants = {
+    visible: (i: number) => ({
+      opacity: 1,
+      transition: {
+        delay: i * 0.025,
+      },
+    }),
+    hidden: { opacity: 0 },
+  };
+
   return (
-    <ul className="flex flex-col gap-4">
-      {tracks.map((track, index) => (
-        <li key={track.id} className="flex gap-4">
-          <p className="font-bold text-md self-center">{index + 1}</p>
-          <Image
-            src={track.album.images[1].url}
-            alt="a track image"
-            width={50}
-            height={50}
-            className="rounded-xl w-24"
-          />
-          <div className="flex justify-between w-full">
-            <div className="flex-col mr-">
-              <p className="text-xl font-semibold">{track.name}</p>
-              <p className="font-extralight">
-                {getArtistString(track.artists)}
-              </p>
-              <p className="font-extralight">
-                BPM/Tempo: {Math.round(track.tempo)}
-              </p>
-            </div>
+    <AnimatePresence>
+      <ul className="flex flex-col gap-4">
+        {tracks.map((track, index) => (
+          <motion.li
+            key={track.id}
+            className="flex gap-4"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            custom={index}
+          >
+            <p className="font-bold text-md self-center">{index + 1}</p>
+            <Image
+              src={track.album.images[1].url}
+              alt="a track image"
+              width={50}
+              height={50}
+              className="rounded-xl w-24"
+            />
+            <div className="flex justify-between w-full">
+              <div className="flex-col mr-">
+                <p className="text-xl font-semibold">{track.name}</p>
+                <p className="font-extralight">
+                  {getArtistString(track.artists)}
+                </p>
+                <p className="font-extralight">
+                  BPM/Tempo: {Math.round(track.tempo)}
+                </p>
+              </div>
             <a href={track.uri}>track link</a>
-          </div>
-        </li>
-      ))}
-    </ul>
+            </div>
+          </motion.li>
+        ))}
+      </ul>
+    </AnimatePresence>
   );
 }
