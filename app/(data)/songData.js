2385b971a3b718e737d27477d1e857c780a77002
Kevin Le <iamkevinvle@gmail.com>
2023-10-11 00:08:09 -0700

    edit chatGPT prompt to simulate a music reviewer

---
+++ app/(data)/songData.js
@@ -0,0 +1,191 @@
+export const songData = [
+  { name: 'astrid', artists: 'glaive', album: 'cypress grove' },
+  { name: 'Tick Tock', artists: 'Joji', album: 'Nectar' },
+  { name: 'Do Not Disturb', artists: 'Drake', album: 'More Life' },
+  {
+    name: 'Mean',
+    artists: '$NOT, Flo Milli',
+    album: 'Beautiful Havoc',
+  },
+  { name: 'GOSHA', artists: '$NOT', album: '- TRAGEDY +' },
+  {
+    name: 'Demons (feat. Fivio Foreign & Sosa Geek)',
+    artists: 'Drake, Fivio Foreign, Sosa Geek',
+    album: 'Dark Lane Demo Tapes',
+  },
+  {
+    name: 'Fair Trade (with Travis Scott)',
+    artists: 'Drake, Travis Scott',
+    album: 'Certified Lover Boy',
+  },
+  {
+    name: 'Tuscan Leather',
+    artists: 'Drake',
+    album: 'Nothing Was The Same (Deluxe)',
+  },
+  {
+    name: 'Wants and Needs (feat. Lil Baby)',
+    artists: 'Drake, Lil Baby',
+    album: 'Scary Hours 2',
+  },
+  { name: 'Revenge', artists: '$NOT', album: 'Beautiful Havoc' },
+  { name: 'Peach', artists: 'Kevin Abstract', album: 'ARIZONA BABY' },
+  {
+    name: 'When To Say When',
+    artists: 'Drake',
+    album: 'Dark Lane Demo Tapes',
+  },
+  {
+    name: 'OLD NEWS (feat. Baird)',
+    artists: 'BROCKHAMPTON, Baird',
+    album: 'ROADRUNNER: NEW LIGHT, NEW MACHINE',
+  },
+  { name: 'bugging!', artists: 'brakence', album: 'hypochondriac' },
+  {
+    name: 'Lost Me',
+    artists: 'Internet Money, iann dior, Lil Skies, Lil Mosey',
+    album: 'B4 The Storm (Complete Edition)',
+  },
+  {
+    name: 'Mona Lisa (Spider-Man: Across the Spider-Verse)',
+    artists: 'Dominic Fike',
+    album: 'Mona Lisa (Spider-Man: Across the Spider-Verse)',
+  },
+  {
+    name: 'In The Bible (with Lil Durk & Giveon)',
+    artists: 'Drake, Lil Durk, Giveon',
+    album: 'Certified Lover Boy',
+  },
+  {
+    name: 'Pound Cake / Paris Morton Music 2',
+    artists: 'Drake, JAY-Z',
+    album: 'Nothing Was The Same (Deluxe)',
+  },
+  {
+    name: 'Champagne Poetry',
+    artists: 'Drake',
+    album: 'Certified Lover Boy',
+  },
+  { name: 'Jaded', artists: 'Drake', album: 'Scorpion' },
+  {
+    name: 'Fck Boys',
+    artists: 'Blxst, Russ',
+    album: 'Just for Clarity',
+  },
+  {
+    name: 'Go Crazy',
+    artists: 'Chris Brown, Young Thug',
+    album: 'Slime & B',
+  },
+  {
+    name: 'Change (with Kehlani)',
+    artists: 'Arin Ray, Kehlani',
+    album: 'Phases II - EP',
+  },
+  {
+    name: 'Garden - Extended',
+    artists: 'Jedwill, camoufly',
+    album: 'Garden (Extended)',
+  },
+  { name: 'Losses', artists: 'Drake', album: 'Dark Lane Demo Tapes' },
+  {
+    name: 'Ex For A Reason (with JT from City Girls)',
+    artists: 'Summer Walker, JT, City Girls',
+    album: 'Still Over It',
+  },
+  { name: '2hrs', artists: 'tobi lou', album: '2hrs' },
+  {
+    name: 'Jerry Sprunger (with T-Pain)',
+    artists: 'Tory Lanez, T-Pain',
+    album: 'Chixtape 5',
+  },
+  {
+    name: "I'LL TAKE YOU ON (feat. Charlie Wilson)",
+    artists: 'BROCKHAMPTON, Charlie Wilson',
+    album: 'ROADRUNNER: NEW LIGHT, NEW MACHINE',
+  },
+  {
+    name: 'Chicago Freestyle (feat. Giveon)',
+    artists: 'Drake, Giveon',
+    album: 'Dark Lane Demo Tapes',
+  },
+  {
+    name: 'The Remorse',
+    artists: 'Drake',
+    album: 'Certified Lover Boy',
+  },
+  {
+    name: 'SEASIDE (feat. iann dior)',
+    artists: 'Lil Tecca, iann dior',
+    album: 'We Love You Tecca 2',
+  },
+  { name: 'Off The Grid', artists: 'Kanye West', album: 'Donda' },
+  {
+    name: 'Chosen (feat. Ty Dolla $ign)',
+    artists: 'Blxst, Tyga, Ty Dolla $ign',
+    album: 'No Love Lost (Deluxe)',
+  },
+  {
+    name: 'Dior (with Gunna) - Remix',
+    artists: 'Pop Smoke, Gunna',
+    album: 'Meet The Woo 2 (Deluxe)',
+  },
+  {
+    name: 'WINDOWS (feat. SoGone SoFlexy)',
+    artists: 'BROCKHAMPTON, SoGone SoFlexy',
+    album: 'ROADRUNNER: NEW LIGHT, NEW MACHINE',
+  },
+  {
+    name: 'Whipski (feat. Lil Skies & Internet Money)',
+    artists: '$NOT, Lil Skies, Internet Money',
+    album: 'Whipski (feat. Lil Skies & Internet Money)',
+  },
+  { name: 'Fear', artists: 'Drake', album: 'Fear' },
+  {
+    name: 'Losing Focus',
+    artists: 'Bryson Tiller',
+    album: 'A N N I V E R S A R Y (Deluxe)',
+  },
+  {
+    name: 'COUNT ON ME',
+    artists: 'BROCKHAMPTON',
+    album: 'ROADRUNNER: NEW LIGHT, NEW MACHINE',
+  },
+  {
+    name: 'Shot For Me',
+    artists: 'Drake',
+    album: 'Take Care (Deluxe)',
+  },
+  { name: 'War', artists: 'Drake', album: 'Dark Lane Demo Tapes' },
+  {
+    name: 'Lemonade (feat. Gunna, Don Toliver & NAV)',
+    artists: 'Internet Money, Gunna, Don Toliver, NAV',
+    album: 'B4 The Storm',
+  },
+  {
+    name: 'Preach',
+    artists: 'Drake, PARTYNEXTDOOR',
+    album: "If You're Reading This It's Too Late",
+  },
+  {
+    name: 'burbank house',
+    artists: 'tsubi club',
+    album: 'burbank house',
+  },
+  { name: '2009', artists: 'glaive', album: 'cypress grove' },
+  {
+    name: 'Politics & Violence',
+    artists: 'Dominic Fike',
+    album: 'What Could Possibly Go Wrong',
+  },
+  {
+    name: 'WAIT FOR U (feat. Drake & Tems)',
+    artists: 'Future, Drake, Tems',
+    album: 'I NEVER LIKED YOU',
+  },
+  {
+    name: 'Over My Dead Body',
+    artists: 'Drake',
+    album: 'Take Care (Deluxe)',
+  },
+];
--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,26 +1,30 @@
 import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
+import { songData } from '../../(data)/songData.js';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(request: Request) {
-  const body = await request.json();
   const chatCompletion = await openai.chat.completions.create({
     messages: [
       {
         role: 'system',
-        content:
-          'You are a Spotify AI Assistant tasked with helping users make personalized Spotify playlists when given a genre of music and/or 3 artists that they like.',
+        content: `ChatGPT, please act as Anthony Fantano, a popular music reviewer, for the rest of this discussion and answer the following question in a manner consistent with his reviewing style.
+
+        ChatGPT: [Response as Anthony Fantano]`,
       },
       {
         role: 'user',
-        content: `Generate me a 10 song playlist consisting of songs from artists: ${body.query}, in an array of JSON objects with the following shape example: {title: 'Goodies', artist: Ciara} `,
+        content: `acting as anthony fantano, a popular music reviewer, use this list of my top tracks: ${JSON.stringify(
+          songData
+        )} to judge my music taste and make inferences on it. write 4 paragraphs critiquing my music taste. Start every response with the phrase: "Hi everyone. Anthony Fantano here, the internet's busiest music nerd, and it's time for a review of Kevin's Le's music taste. at the end, give my music taste a range score from 0 - 10 with the difference in range being no greater than 1. any number can be prefixed with the adjectives "light", "decent", or "strong", where a "light 3" would indicate approximately 3.2, and a "strong 3" would be approximately 3.8. write the ending score in the format: "i'm feeling a light 3 to a strong 4 on this one". use numerous examples of songs in this array to personalize the message. act in a playful but critical tone.
+        `,
       },
     ],
     model: 'gpt-3.5-turbo',
   });
-
+  console.log(chatCompletion.usage, 'choices');
   return NextResponse.json({ data: chatCompletion.choices });
 }
