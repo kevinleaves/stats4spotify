1664c542e548065d39aad54eef87552484e1273f
Kevin Le <iamkevinvle@gmail.com>
2023-10-14 14:32:02 -0700

    prevent user from double clicking button/sending multiple requests client side

--- app/(client)/(widgets)/(input)/Chat.tsx
+++ app/(client)/(widgets)/(input)/Chat.tsx
@@ -4,6 +4,7 @@ import { streamResponse } from '../../(services)/openai-service';
 import { getUsersTopItems } from '@/lib/spotify';
 import { useQuery } from '@tanstack/react-query';
 import Loading from '../../tracks/top/loading';
+import LinearProgress from '@mui/material/LinearProgress';
 
 function useUsersTopArtists() {
   // hook used to data fetch users top artists from a client component
@@ -14,13 +15,23 @@ function useUsersTopArtists() {
 }
 
 export default function Chat() {
-  // const [isLoading, setIsLoading] = useState(false);
+  const [isGeneratingResponse, setIsGeneratingResponse] = useState(false);
   const [results, setResults] = useState('');
 
-  async function onSubmit(event: React.SyntheticEvent) {
+  async function handleSubmit(event: React.SyntheticEvent) {
+    // clear results on subsquent clicks
+    setResults('');
     event.preventDefault();
-    // setIsLoading(true);
+    // disable the button to prevent users from sending a request while one is currently active
+    setIsGeneratingResponse(true);
     const res = await streamResponse();
+
+    // double click does nothing because of our rate limited backend
+    if (res.status === 429) {
+      setResults('');
+      return;
+    }
+
     const reader = res.body?.pipeThrough(new TextDecoderStream()).getReader();
 
     let done = false;
@@ -31,19 +42,30 @@ export default function Chat() {
         setResults((prev) => prev + value);
       }
     }
+
+    // allow user to click the button again
+    setIsGeneratingResponse(false);
   }
 
   return (
     <section className="w-full flex flex-col items-center">
-      {/* {isLoading ? <Loading /> : <div>not loading</div>} */}
-
       <div className="whitespace-break-spaces w-full">{results}</div>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit}>
+        <div className="w-24"></div>
         <button
           type="submit"
-          className="justify-center items-center hover:underline hover mt-4 p-4 border-2 rounded-lg"
+          className="justify-center items-center hover:underline hover mt-4 p-4 border-2 rounded-lg w-full"
+          disabled={isGeneratingResponse}
         >
-          click here to have spotifyGPT/fantanobot judge your music taste
+          {isGeneratingResponse ? (
+            <LinearProgress
+              sx={{
+                width: '100%',
+              }}
+            />
+          ) : (
+            'click here to have spotifyGPT/fantanobot judge your music taste'
+          )}
         </button>
       </form>
     </section>
