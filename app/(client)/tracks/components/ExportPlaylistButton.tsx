ac1bf08261b4196a176c695f9242498a77337fd8
Kevin Le <iamkevinvle@gmail.com>
2023-10-26 11:04:42 -0700

    export playlist button has loading spinner + links to exported playlist

--- app/(client)/tracks/components/ExportPlaylistButton.tsx
+++ app/(client)/tracks/components/ExportPlaylistButton.tsx
@@ -1,5 +1,8 @@
 'use client';
+import { useState } from 'react';
 import { createPlaylist, addItemsToPlaylist } from '@/lib/spotify';
+import LinearProgress from '@mui/material/LinearProgress';
+import Link from 'next/link';
 
 interface Props {
   headerText: string;
@@ -7,20 +10,49 @@ interface Props {
 }
 
 export default function ExportPlaylistButton({ headerText, uris }: Props) {
+  const [isPlaylistGenerated, setIsPlaylistGenerated] = useState(false);
+  const [playlistId, setPlaylistId] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const baseURL = 'https://open.spotify.com/playlist';
+
   const exportPlaylist = async (headerText: string, uris: string[] = []) => {
+    setIsLoading(true);
     const res: SpotifyApi.CreatePlaylistResponse = await createPlaylist(
       headerText
     );
-    const { id: playlistId } = res;
-    await addItemsToPlaylist(playlistId, uris);
+
+    const { id } = res;
+    setPlaylistId(id);
+    await addItemsToPlaylist(id, uris);
+    setIsLoading(false);
+    setIsPlaylistGenerated(true);
   };
 
   return (
-    <button
-      className={'bg-green-600 w-2/6 h-12 rounded-lg'}
-      onClick={() => exportPlaylist(headerText, uris)}
-    >
-      create playlist
-    </button>
+    <div className="flex justify-center h-12 w-48 items-center">
+      {isLoading ? (
+        <LinearProgress
+          sx={{
+            width: '100%',
+          }}
+        />
+      ) : isPlaylistGenerated ? (
+        <Link
+          href={`${baseURL}/${playlistId}`}
+          className={'bg-blue-200  rounded-lg hover:underline p-4'}
+          target="_blank"
+        >
+          view playlist
+        </Link>
+      ) : (
+        <button
+          className={'bg-green-600  rounded-lg hover:underline p-4'}
+          onClick={() => exportPlaylist(headerText, uris)}
+        >
+          create playlist
+        </button>
+      )}
+    </div>
   );
 }
