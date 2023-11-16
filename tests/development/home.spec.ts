186f905a74cab239c1135c2ba25b8fd282da574e
Kevin Le <iamkevinvle@gmail.com>
2023-11-15 23:40:46 -0800

    remove login steps in dev homepage beforeEach testing

--- tests/development/home.spec.ts
+++ tests/development/home.spec.ts
@@ -1,41 +1,16 @@
 import { test, expect } from '@playwright/test';
-import { chromium } from '@playwright/test';
 
-const authFile = 'playwright/.auth/devUser.json';
-
-test.beforeEach('log in', async ({ page }) => {
-  // const browser = await chromium.launch();
-  // const page = await browser.newPage();
-
-  await page.goto(
-    'http://localhost:3000/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F'
-  );
-  await page.getByRole('button', { name: 'Sign in with Spotify' }).click();
-
-  //! adding waitForLoadState fixes flaky button press
-  await page.waitForLoadState('networkidle'); // <-  until there are no network connections for at least 500 ms.
-  //! boilerplate
-
-  //* Wait until the page receives the cookies.
-  //* Sometimes login flow sets cookies in the process of several redirects.
-  //* Wait for the final URL to ensure that the cookies are actually set.
-  await page.waitForURL('**/', { timeout: 30000 });
-  // Alternatively, you can wait until the page reaches a state where all cookies are set.
-
-  // Wait for network to be idle, if we save storage too early, needed storage values might not yet be available
-  await page.waitForLoadState('networkidle');
-
-  // End of authentication steps.
-  // await page.context().storageState({ path: authFile });
-
-  // assert
-  await expect(page.getByRole('button', { name: 'Sign out' })).toBeVisible({
-    timeout: 30000,
-  });
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
 });
 
 test.describe('dev homepage tests', () => {
-  // test.use({ storageState: authFile });
+  test('user should be logged in after setup complete', async ({ page }) => {
+    await expect(page.getByRole('button', { name: 'Sign out' })).toBeVisible({
+      timeout: 30000,
+    });
+  });
+
   test('page header is visible', async ({ page }) => {
     // header is visible
     await expect(
