518d48985c8a8ea4e2ba0fa0a52cb1cf137be9d0
Kevin Le <iamkevinvle@gmail.com>
2023-11-17 21:34:15 -0800

    artists page but client component

---
+++ app/(client)/artists/top/ClientArtistPage.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'next/navigation';
+import { getUsersTopItems } from '@/lib/spotify';
+import Loading from './loading';
+import ArtistList from '../_components/ArtistList';
+import { Typography } from '@mui/material';
+import ArtistDetailsModal from '../../cartists/_components/ArtistDetailsModal';
+
+interface Props {}
+
+export function ClientArtistPage({}: Props) {
+  const searchParams = useSearchParams();
+  const timeRange = searchParams.get('timeRange');
+
+  let headerText = 'Top Artists: last 4 weeks';
+
+  switch (timeRange) {
+    case 'short_term':
+      headerText = 'Top Artists: last 4 weeks';
+      break;
+    case 'medium_term':
+      headerText = 'Top Artists: last 6 months';
+      break;
+    case 'long_term':
+      headerText = 'Top Artists: all time';
+      break;
+    default:
+      break;
+  }
+
+  function useUsersTopArtists() {
+    // hook used to data fetch users top artists from a client component
+    return useQuery({
+      queryKey: ['artists', timeRange],
+      queryFn: () => {
+        return getUsersTopItems('artists', timeRange, 50);
+      },
+      // enabled: false,
+    });
+  }
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedArtist, setSelectedArtist] =
+    useState<SpotifyApi.ArtistObjectFull | null>(null);
+
+  const { data, isError, error, isFetching } = useUsersTopArtists();
+
+  if (isFetching) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  const { items: artists } = data;
+
+  // sort method mutates original array, so we copy it
+  let sorted = artists?.slice().sort((a, b) => {
+    return b.popularity - a.popularity;
+  });
+
+  return (
+    <main className="flex flex-col justify-center items-center gap-4 sm:max-lg:gap-8">
+      <Typography
+        variant="h2"
+        sx={{
+          '@media (min-width: 1024px)': {
+            fontSize: '1.875rem',
+            lineHeight: '2.25rem',
+          },
+          fontSize: '1.125rem',
+          lineHeight: '1.75rem',
+          fontWeight: 700,
+          letterSpacing: '-0.05em',
+        }}
+      >
+        {headerText}
+      </Typography>
+      {artists?.length === 0 ? (
+        <div className="flex flex-col items-center w-full sm:w-1/2">
+          {`It seems that you haven't heard enough music to calculate any
+          favorites from it. Try another time range or listen to some more music
+          and try again later!`}
+        </div>
+      ) : (
+        <ArtistList
+          artists={artists}
+          setIsModalOpen={setIsModalOpen}
+          isModalOpen={isModalOpen}
+          setSelectedArtist={setSelectedArtist}
+        />
+      )}
+      {isModalOpen ? (
+        <ArtistDetailsModal
+          selectedArtist={selectedArtist}
+          setIsModalOpen={setIsModalOpen}
+          isModalOpen={isModalOpen}
+          setSelectedArtist={setSelectedArtist}
+        />
+      ) : null}
+    </main>
+  );
+}
---
+++ app/(client)/cartists/page.tsx
@@ -0,0 +1,11 @@
+import { ClientArtistPage } from '../artists/top/ClientArtistPage';
+
+interface Props {}
+
+export default function CArtists({}: Props) {
+  return (
+    <>
+      <ClientArtistPage />
+    </>
+  );
+}
